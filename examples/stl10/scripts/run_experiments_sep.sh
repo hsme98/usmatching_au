aaaa3a5d0dbf71a857452ac325078e07815180dd
hsme98 <melihcanerol98@gmail.com>
2024-03-06 16:58:23 -0500

    updated

--- examples/stl10/scripts/run_experiments_sep.sh
+++ examples/stl10/scripts/run_experiments_sep.sh
@@ -11,6 +11,6 @@ module load cuda/11.8
 source activate distil_env6
 
 SHARED=""
-TRANSFORMS="/home/gridsan/herol/usmatching_au/examples/stl10/transforms/transforms.py"
+TRANSFORMS="/home/gridsan/herol/usmatching_au/examples/stl10/transforms/transforms_sep.py"
 
 python /home/gridsan/herol/usmatching_au/examples/stl10/main_multi.py $SHARED --transforms $TRANSFORMS --iter $SLURM_ARRAY_TASK_ID
--- examples/stl10/scripts/run_experiments_shared.sh
+++ examples/stl10/scripts/run_experiments_shared.sh
@@ -10,7 +10,7 @@ module load anaconda/2022a
 module load cuda/11.8
 source activate distil_env6
 
-SHARED=""
-TRANSFORMS="/home/gridsan/herol/usmatching_au/examples/stl10/transforms/transforms.py"
+SHARED="--shared"
+TRANSFORMS="/home/gridsan/herol/usmatching_au/examples/stl10/transforms/transforms_sep.py"
 
 python /home/gridsan/herol/usmatching_au/examples/stl10/main_multi.py $SHARED --transforms $TRANSFORMS --iter $SLURM_ARRAY_TASK_ID
