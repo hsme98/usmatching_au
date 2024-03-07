89bf21569609fd65d618a19828ad217ac385001c
hsme98 <melihcanerol98@gmail.com>
2024-03-06 22:55:44 -0500

    fixed scripts

--- examples/stl10/scripts/run_experiments_sep.sh
+++ examples/stl10/scripts/run_experiments_sep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -o train_teacher.log-%j-%a
-#SBATCH -a 10  # Adjust this to match the number of files
+#SBATCH -a 1-10  # Adjust this to match the number of files
 #SBATCH -c 4
 #SBATCH --gres=gpu:volta:1
 
