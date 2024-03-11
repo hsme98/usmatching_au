f06b3706121740e2181701329918615888ab41ed
hsme98 <melihcanerol98@gmail.com>
2024-03-11 19:56:55 -0400

    updated scripts

--- examples/stl10/scripts/run_experiments.sh
+++ examples/stl10/scripts/run_experiments.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH -o train_teacher.log-%j-%a
-#SBATCH -a 1-10  # Adjust this to match the number of files
+#SBATCH -a 1-4  # Adjust this to match the number of files
 #SBATCH -c 4
 #SBATCH --gres=gpu:volta:1
 
@@ -10,7 +10,9 @@ module load anaconda/2022a
 module load cuda/11.8
 source activate distil_env6
 
-SHARED=""
+SHARED="--shared"
 TRANSFORMS="/home/gridsan/herol/usmatching_au/examples/stl10/transforms/transforms.py"
+AUG="--aug double"
+FOLD="--result_folder /home/gridsan/herol/usmatching_au/experiments_0"
 
-python /home/gridsan/herol/usmatching_au/examples/stl10/main_multi.py $SHARED --transforms $TRANSFORMS --iter $SLURM_ARRAY_TASK_ID
+python /home/gridsan/herol/usmatching_au/examples/stl10/main_multi.py $SHARED $AUG --transforms $TRANSFORMS --iter $SLURM_ARRAY_TASK_ID $FOLD $AUG
